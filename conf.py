01879b2419ce2aebd0afa8f82f37325f38cf5632
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-09-05 22:15:55 -0300

    new URL

--- conf.py
+++ conf.py
@@ -99,7 +99,7 @@ THUMBNAIL_SIZE = 180
 
 # Data about this site
 BLOG_TITLE = "Nikola"
-BLOG_URL = "http://nikola.ralsina.com.ar/"
+BLOG_URL = "http://getnikola.com/"
 BLOG_EMAIL = "ralsina@netmanagers.com.ar"
 BLOG_DESCRIPTION = "A blog about Nikola, the static website generator."
 BLOG_AUTHOR = "Roberto Alsina"
@@ -114,7 +114,7 @@ LICENSE = """
     src="http://i.creativecommons.org/l/by-nc-sa/2.5/ar/88x31.png"></a>"""
 
 # A small copyright notice for the page footer
-CONTENT_FOOTER = '''Contents © 2012-2013 <a href="mailto:ralsina@kde.org">Roberto Alsina</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="http://nikola.ralsina.com.ar">Powered by Nikola</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+CONTENT_FOOTER = '''Contents © 2012-2013 <a href="mailto:ralsina@kde.org">Roberto Alsina</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="http://getnikola.com">Powered by Nikola</a>&nbsp;&nbsp;|&nbsp;&nbsp;
 <a href="https://twitter.com/intent/follow?original_referer=http%3A%2F%2Fplatform.twitter.com%2Fwidgets%2Ffollow_button.1333526973.html&region=follow_link&screen_name=nikolagenerator&source=followbutton&variant=2.0">Follow Nikola on twitter</a>&nbsp;&nbsp;|&nbsp;&nbsp;Theme is <a href="http://bootswatch.com/cerulean/">Cerulean</a>
 '''
 
