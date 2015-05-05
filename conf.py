b4514bf91290e3960d501a5d0eb6e3598050290d
Roberto Alsina <ralsina@netmanagers.com.ar>
2015-05-05 12:13:30 -0300

    updated conf

--- conf.py
+++ conf.py
@@ -108,7 +108,7 @@ WRITE_TAG_CLOUD = True
 
 # Data about this site
 BLOG_TITLE = "Nikola"
-BLOG_URL = "http://getnikola.com/"
+SITE_URL = "http://getnikola.com/"
 BLOG_EMAIL = "info@getnikola.com"
 BLOG_DESCRIPTION = "A blog about Nikola, the static website generator."
 BLOG_AUTHOR = "Roberto Alsina"
